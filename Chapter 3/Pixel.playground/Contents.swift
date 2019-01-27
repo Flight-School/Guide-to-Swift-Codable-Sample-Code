450b736f6a016fb9e250184dcb550f20f2a86ec1
Mattt <mattt@me.com>
2019-01-27 07:44:19 -0800

    Change playground target platform to macOS

--- Chapter 3/Pixel.playground/Contents.swift
+++ Chapter 3/Pixel.playground/Contents.swift
@@ -1,8 +1,7 @@
 import Foundation
 
 let encoder = JSONEncoder()
-encoder.userInfo[.colorEncodingStrategy] =
-    ColorEncodingStrategy.hexadecimal(hash: true)
+encoder.userInfo[.colorEncodingStrategy] = ColorEncodingStrategy.hexadecimal(hash: true)
 
 let cyan = Pixel(red: 0, green: 255, blue: 255)
 let magenta = Pixel(red: 255, green: 0, blue: 255)
@@ -11,4 +10,5 @@ let black = Pixel(red: 0, green: 0, blue: 0)
 
 let json = try! encoder.encode([cyan, magenta, yellow, black])
 let string = String(data: json, encoding: .utf8)!
+
 print(string)
